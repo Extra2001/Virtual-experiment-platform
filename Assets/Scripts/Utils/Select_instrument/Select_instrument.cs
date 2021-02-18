cb856ae214d02e2a37a5d3e43c01761a0fd15c33
flash123456789 <918127890@qq.com>
2021-02-18 21:43:38 +0800

    zjf

---
+++ Assets/Scripts/Utils/Select_instrument/Select_instrument.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+
+namespace UnityEngine.UI.Extensions.Examples.FancyScrollViewExample03
+{
+    class Select_instrument : MonoBehaviour
+    {
+        [SerializeField] ScrollView scrollView = default;
+
+        void Start()
+        {
+            var items = Enumerable.Range(0, 20)
+                .Select(i => new ItemData($"Cell {i}"))
+                .ToArray();
+
+            scrollView.UpdateData(items);
+            scrollView.SelectCell(0);
+        }
+    }
+}
+

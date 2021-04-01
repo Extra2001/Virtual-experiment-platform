14553207c349f2d16920d799089d73cd894ecfac
wsmitpwtind <wsmitpwtind@163.com>
2021-04-01 16:27:49 +0800

    qwq

--- Assets/Scripts/zk(magic_bugs_here,dont touch)/adsorb_ruler.cs
+++ Assets/Scripts/zk(magic_bugs_here,dont touch)/adsorb_ruler.cs
@@ -1,41 +1,52 @@
-﻿using System.Collections;
+using HT.Framework;
+using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class adsorb_ruler : MonoBehaviour
+public class adsorb_ruler : HTBehaviour
 {
+    //启用自动化
+    protected override bool IsAutomate => true;
+    
     public float t = 15.0f;
     public bool Doing = true;
+
+
+    private GameObject[] m_Desk;
     // Start is called before the first frame update
     void Start()
     {
+        GameObject RU = GameObject.Find("ruler");
+        m_Desk = GameObject.FindGameObjectsWithTag("Measured");
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        GameObject RU = GameObject.Find("ruler");
-        GameObject BO = GameObject.Find("book_0001b");
         if (!Input.GetMouseButton(1))
         {
             if (!Input.GetMouseButton(0))
             {
-                if (Vector3.Distance(RU.transform.position, BO.transform.position) <= 0.3f && Doing)
+                for (int i = 0; i < m_Desk.Length; i++)
                 {
-                    absord();
+                    GameObject RU = m_Desk[i];
+                    if (Vector3.Distance(RU.transform.position, BO.transform.position) <= 0.3f && Doing)
+                    {
+                        transform.position += (BO.transform.position - RU.transform.position) / t;
+                    }
+                    else if (Vector3.Distance(RU.transform.position, BO.transform.position) > 0.3f)
+                    {
+                        Doing = true;
+                    }
                 }
-                else if(Vector3.Distance(RU.transform.position, BO.transform.position) > 0.3f)
-                {
-                    Doing = true;
-                }
-
             }
         }
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "book")
+        if (other.tag == "Measured")
         {
             Doing = false;
         }

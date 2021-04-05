763de284c4398f1d7230670b06d9ba8aece179d0
Extra2001 <jingxutian@outlook.com>
2021-04-05 09:02:40 +0800

    hh

---
+++ Assets/Scripts/Game/UI/Meaurement/UncertaintyBackNext.cs
@@ -0,0 +1,20 @@
+using HT.Framework;
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UncertaintyBackNext : HTBehaviour
+{
+    public Button NextButton;
+    public Button BackButton;
+
+    private void Start()
+    {
+        BackButton.onClick.AddListener(() =>
+        {
+
+        });
+    }
+}

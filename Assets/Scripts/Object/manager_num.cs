5175dd9bd53a720353087a3821d133125b8d72a6
wsmitpwtind <wsmitpwtind@163.com>
2021-05-07 16:07:54 +0800

    qwq

--- Assets/Scripts/Object/manager_num.cs
+++ Assets/Scripts/Object/manager_num.cs
@@ -4,6 +4,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.PackageManager;
+using UnityEngine.UIElements;
+using UnityEngine.UI.Extensions;
 
 public class manager_num : HTBehaviour
 {
@@ -11,23 +13,9 @@ public class manager_num : HTBehaviour
     protected override bool IsAutomate => true;
     public int accuracy_rating; // 小数点后多少位
     /// </summary>
-    GameObject f1;
-    GameObject f2;
-    GameObject f3;
-    GameObject f4;
-    GameObject f5;
-    GameObject f6;
-    GameObject f7;
     // Start is called before the first frame update
     void Start()
     {
-        f1 = this.transform.Find("1").gameObject;
-        f2 = this.transform.Find("1").gameObject;
-        f3 = this.transform.Find("1").gameObject;
-        f4 = this.transform.Find("1").gameObject;
-        f5 = this.transform.Find("1").gameObject;
-        f6 = this.transform.Find("1").gameObject;
-        f7 = this.transform.Find("1").gameObject;
     }
     public void Show_num(double num)
     {
@@ -42,12 +30,17 @@ public class manager_num : HTBehaviour
         }
         else
         {
-            //string s = ToString(num_int)
+            string s = num_int.ToString();
+            for (int i = s.Length-1; i > -1; i--)
+            {
+                this.transform.Find((i+1).ToString()).gameObject.GetComponent<show_num>().ChangeTheNum((int)s[s.Length-i-1]-48);
+            }
         }
     }
     // Update is called once per frame
     void Update()
     {
-        //f1.GetComponent<show_num>().ChangeTheNum(1);
+       Show_num(31);
+       
     }
 }

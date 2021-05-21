7aa86909c957913798f885b1267b10124e1e5e04
cbj0 <wojiaocbj@163.com>
2021-05-21 22:41:20 +0800

    ......

--- Assets/Scripts/Utils/Calculate/Calculator.cs
+++ Assets/Scripts/Utils/Calculate/Calculator.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -13,16 +14,30 @@ using symdiff = MathNet.Symbolics.Calculus;
 using symfuncs = MathNet.Symbolics.Function;
 
 public static class StaticMethods {
+    public static readonly HashSet<string> keywords = new HashSet<string>(){
+            "pi","e","abs","acos","asin","atan","sin","cos","tan","cot","sec","csc","j","sqrt","pow","sinh","cosh","tanh","exp","ln","lg"
+        };
     public static double Average(IEnumerable<double> data) {
         return MathNet.Numerics.Statistics.Statistics.Mean(data);
     }
-    public static double Variance(IEnumerable<double> data) {
+    public static double VarianceN(IEnumerable<double> data) {//除N的方差
         return MathNet.Numerics.Statistics.Statistics.PopulationVariance(data);
     }
-    public static double Moment(IEnumerable<double> data) {
-        throw new NotImplementedException();
+    public static double Variance(IEnumerable<double> data) {
+        return MathNet.Numerics.Statistics.Statistics.Variance(data);
     }
-    public static string NumberFormat(double Input) {
+    public static double StdDev(IEnumerable<double> data) {//除N-1的标准差
+        return MathNet.Numerics.Statistics.Statistics.StandardDeviation(data);
+    }
+    public static double CenterMoment(IEnumerable<double> data, int k) {//k阶原点矩
+        double ans = 0;
+        int n = 0;
+        foreach(var item in data) {
+            ans += Math.Pow(item, k); n++;
+        }
+        return ans /= n;
+    }
+    public static string NumberFormat(double Input) {//格式化输出float
         string Output;
 
         if(Math.Abs(Input) > 0.01 && Math.Abs(Input) < 1000) {
@@ -34,8 +49,65 @@ public static class StaticMethods {
         else {
             Output = Input.ToString("E");
         }
-
         return Output;
+    }
+    public static (double avg, double ua, double u) CalcUncertain(IEnumerable<double> data, double ub) {
+        //输入:测量数据data 仪器B类不确定度ub
+        //返回:(主值,A类不确定度,合成不确定度)
+        int n = 0;
+        double sum1 = 0, sum2 = 0;
+        foreach(var item in data) {
+            sum1 += item; n++;
+        }
+        double average = sum1 / n;
+        foreach(var item in data) {
+            sum2 += (item - average) * (item - average) / (n * (n - 1));
+        }
+        return (average, Math.Sqrt(sum2), Math.Sqrt(sum2 + ub * ub));
+    }
+    public static bool ValidVarname(string v) {//检查v是否能作为合法物理量名
+        if(keywords.Contains(v)) {//函数列表有 直接否
+            return false;
+        }
+        else {
+            if(!char.IsLetter(v[0]) || Regex.IsMatch(v, @"[^a-zA-Z0-9]")) {//只允许A-Za-z0-9
+                return false;
+            }
+            return true;
+        }
+    }
+    public static bool ValidExpression(string v) {//检查表达式v是否合法(后续要调)
+        try {
+            var expr0 = symexpr.Parse(v);
+            return true;
+        }
+        catch(Exception) {
+            return false;
+        }
+    }
+    public static string PostResponse(string url, string data) {//发送web请求
+        try {
+            string r = default;
+            HttpWebRequest req = WebRequest.CreateHttp(url);
+            req.Method = "POST";
+            req.KeepAlive = true;
+            req.ContentType = "application/json";
+            using(var s = req.GetRequestStream()) {
+                byte[] buf = Encoding.UTF8.GetBytes(data);
+                s.Write(buf, 0, buf.Length);
+            }
+            using(HttpWebResponse resp = req.GetResponse() as HttpWebResponse) {
+                using(StreamReader sr = new StreamReader(resp.GetResponseStream(), Encoding.UTF8)) {
+                    r = sr.ReadToEnd();
+                }
+            }
+            return r;
+        }
+        catch(Exception ex) {
+            Console.Error.WriteLine(ex);
+            return null;
+            //throw;
+        }
     }
 }
 public class CalcVariable {
@@ -69,7 +141,7 @@ public class CalcArgs {//一次计算
     public static readonly HashSet<string> keywords = new HashSet<string>(){
             "pi","e","abs","acos","asin","atan","sin","cos","tan","cot","sec","csc","j","sqrt","pow","sinh","cosh","tanh","exp","ln","lg"
         };
-    public bool ValidVarname(string v) {//检查
+    public bool ValidVarname(string v) {//检查变量类型
         if(keywords.Contains(v)) {
             return false;
         }

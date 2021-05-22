23afccfe1b0be4793de86585f9655db64595746a
Extra2001 <jingxutian@outlook.com>
2021-05-22 16:25:08 +0800

    hh

--- Assets/Scripts/Game/UI/Bag/BagSelectorAnimate.cs
+++ Assets/Scripts/Game/UI/Bag/BagSelectorAnimate.cs
@@ -10,13 +10,20 @@ public class BagSelectorAnimate : MonoBehaviour
 {
     public void Show()
     {
+        CancelInvoke();
         UIShowHideHelper.ShowFromUp(gameObject, 50);
         gameObject.rectTransform().DOPunchRotation(new Vector3(0, 0, 20), 0.5f)
             .SetEase(Ease.OutExpo);
+        Invoke(nameof(ResetRotation), .55f);
     }
 
     public void Hide()
     {
         UIShowHideHelper.HideToUp(gameObject);
+    }
+
+    private void ResetRotation()
+    {
+        gameObject.rectTransform().rotation = new Quaternion();
     }
 }

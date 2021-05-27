149265091774c9bb291495374fb9383d2b86811e
wsmitpwtind <wsmitpwtind@163.com>
2021-05-27 23:48:17 +0800

    qwq

---
+++ Assets/Scripts/Game/Instruments/AmmeterInstruction.cs
@@ -0,0 +1,42 @@
+using HT.Framework;
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[EntityResource(null, null, "Instruments/Ammeter/Ammeter")]
+
+public class AmmeterInstruction : InstrumentBase
+{
+    public override string InstName => "电流表";
+
+    public override double URV => 15;
+
+    public override double LRV => -3;
+
+    public override double ErrorLimit => 0.5; //忘了
+
+    public override double RandomErrorLimit { get; set; }
+    public override double MainValue { get; set; }
+
+    public override string Unit => "安培";
+
+    public override string UnitSymbol => "A";
+
+    public override string previewImagePath => "Instruments/Thermometer/thermometer_preview";
+
+    public override double GetMeasureResult()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void InstReset()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ShowValue(double value)
+    {
+        throw new System.NotImplementedException();
+    }
+}

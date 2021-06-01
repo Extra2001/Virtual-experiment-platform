ee68ff58956810ee6d7671c8609fd98825ba4e85
wsmitpwtind <wsmitpwtind@163.com>
2021-06-01 16:14:50 +0800

    qwq

--- Assets/Scripts/Object/thermometer_main.cs
+++ Assets/Scripts/Object/thermometer_main.cs
@@ -8,11 +8,17 @@ public class thermometer_main : HTBehaviour
 {
     //启用自动化
     protected override bool IsAutomate => true;
-    
-    // Start is called before the first frame update
+
+    // Start is called before the first frame update、
+    public void ShowNum(float num)
+    {
+        transform.Find("Cylinder001").transform.DOScaleZ(0.67f+num*1.1f/50f, 1f).SetEase(Ease.OutExpo);
+    }
+
+
     void Start()
     {
-        
+        ShowNum(23);
     }
 
     // Update is called once per frame

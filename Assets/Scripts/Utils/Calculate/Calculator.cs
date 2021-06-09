7de35c70e19873bf7fe30689b8bf8ee2ec39956e
cbj0 <wojiaocbj@163.com>
2021-06-09 21:33:15 +0800

    有效数字

--- Assets/Scripts/Utils/Calculate/Calculator.cs
+++ Assets/Scripts/Utils/Calculate/Calculator.cs
@@ -12,7 +12,116 @@ using expr = MathNet.Symbolics.Expression;
 using symexpr = MathNet.Symbolics.SymbolicExpression;
 using symdiff = MathNet.Symbolics.Calculus;
 using symfuncs = MathNet.Symbolics.Function;
-
+public struct CheckFloat {
+    public decimal Value { get; private set; }
+    public decimal TrueValue => Value * (decimal)Math.Pow(10, HiDigit);
+    public int EffectiveDigit { get; private set; }
+    public int LoDigit { get; private set; }
+    public int HiDigit { get; private set; }
+    public CheckFloat(decimal truevalue) : this(truevalue.ToString()) { }
+    public CheckFloat(string value) {
+        EffectiveDigit = Effectiveness(value);
+        if(decimal.TryParse(value, out decimal tmp)) {
+            Value = tmp;
+        }
+        else if(double.TryParse(value, out double tmp2)) {
+            Value = new decimal(tmp2);
+        }
+        else throw new NotSupportedException();
+        if(Value != 0) {
+            HiDigit = (int)Math.Floor(Math.Log10((double)Math.Abs(Value)));
+            LoDigit = HiDigit - EffectiveDigit + 1;
+            if(HiDigit > 0) {
+                for(int i = 0;i < HiDigit;i++) {
+                    Value /= 10;
+                }
+            }
+            else if(HiDigit < 0) {
+                for(int i = 0;i < -HiDigit;i++) {
+                    Value *= 10;
+                }
+            }
+        }
+        else {
+            LoDigit = 0; HiDigit = 0;
+        }
+    }
+    public static double KeepEffective(double d, int n) {//保留n位有效数字
+        if(d == 0.0) return 0;
+        if(d > 1 || d < -1)
+            n = n - (int)Math.Log10(Math.Abs(d)) - 1;
+        else
+            n = n + (int)Math.Log10(1.0 / Math.Abs(d));
+        if(n < 0) {
+            d = (int)(d / Math.Pow(10, 0 - n)) * Math.Pow(10, 0 - n);
+            n = 0;
+        }
+        return Math.Round(d, n);
+    }
+    public CheckFloat KeepEffective(int n) {
+        decimal tmp = decimal.Round(Value, n);
+        return new CheckFloat() { Value = tmp, EffectiveDigit = n + 1, HiDigit = HiDigit, LoDigit = HiDigit - n };
+    }
+    public static decimal KeepEffective(decimal d, int n) {//保留小数点后n位 共n+1位有效数字
+        if(d == 0) return 0;
+        if(d > 1 || d < -1)
+            n = n - (int)Math.Log10((double)Math.Abs(d)) - 1;
+        else
+            n = n + (int)Math.Log10((double)(1 / Math.Abs(d)));
+        if(n < 0) {
+            d = (int)((double)d / Math.Pow(10, -n)) * (decimal)(Math.Pow(10, -n));
+            n = 0;
+        }
+        return Math.Round(d, n);
+    }
+    public static decimal KeepTo(decimal truevalue, int n) {
+        return Math.Truncate(truevalue / (decimal)Math.Pow(10, n)) * (decimal)Math.Pow(10, n);
+    }
+    public static int Effectiveness(string num) {
+        int digits = 0; bool lead0 = true;
+        foreach(var item in num) {
+            if(item == 'E' || item == 'e') {
+                break;
+            }
+            int i = item - '0';
+            if(i >= 0 && i <= 9) {
+                if(i > 0 || (i == 0 && !lead0)) {
+                    digits++; lead0 = false;
+                }
+            }
+        }
+        return Math.Max(digits, 1);
+    }
+    public static CheckFloat operator +(CheckFloat num) {
+        return num;
+    }
+    public static CheckFloat operator -(CheckFloat num) {
+        return new CheckFloat() { EffectiveDigit = num.EffectiveDigit, HiDigit = num.HiDigit, LoDigit = num.LoDigit, Value = -num.Value };
+    }
+    public static CheckFloat operator +(CheckFloat lhs, CheckFloat rhs) {
+        int lo = Math.Max(lhs.LoDigit, rhs.LoDigit);
+        int keep1 = lhs.HiDigit - lo + 1, keep2 = rhs.HiDigit - lo + 1;
+        decimal tmp = lhs.TrueValue + rhs.TrueValue;
+        return new CheckFloat(KeepTo(tmp, lo).ToString($"G{Math.Max(lhs.HiDigit, rhs.HiDigit)}"));
+    }
+    public static CheckFloat operator -(CheckFloat lhs, CheckFloat rhs) {
+        int lo = Math.Max(lhs.LoDigit, rhs.LoDigit);
+        int keep1 = lhs.HiDigit - lo + 1, keep2 = rhs.HiDigit - lo + 1;
+        decimal tmp = lhs.TrueValue - rhs.TrueValue;
+        return new CheckFloat(KeepTo(tmp, lo).ToString($"E{Math.Max(keep1, keep2) - 1}"));
+    }
+    public static CheckFloat operator *(CheckFloat lhs, CheckFloat rhs) {
+        decimal tmp = KeepEffective((lhs.TrueValue * rhs.TrueValue), Math.Min(rhs.EffectiveDigit, lhs.EffectiveDigit));
+        return new CheckFloat(tmp.ToString());
+    }
+    public static CheckFloat operator /(CheckFloat lhs, CheckFloat rhs) {
+        decimal tmp = KeepEffective((lhs.TrueValue / rhs.TrueValue), Math.Min(rhs.EffectiveDigit, lhs.EffectiveDigit));
+        return new CheckFloat(tmp.ToString());
+    }
+    public override string ToString() {
+        return $"值为{TrueValue},{EffectiveDigit}位有效数字,小数部分:{Value}";
+    }
+}
 public static class StaticMethods {
     public static readonly HashSet<string> keywords = new HashSet<string>(){
             "pi","e","abs","acos","asin","atan","sin","cos","tan","cot","sec","csc","j","sqrt","pow","sinh","cosh","tanh","exp","ln","lg"
@@ -65,11 +174,10 @@ public static class StaticMethods {
         }
         return (average, Math.Sqrt(sum2), Math.Sqrt(sum2 + ub * ub));
     }
-    public static double CalcUncertain(double ua, double ub)
-    {
+    public static double CalcUncertain(double ua, double ub) {
         //输入:A类不确定度ua 仪器B类不确定度ub
         //返回:合成不确定度
-        return Math.Sqrt(ua*ua + ub * ub);
+        return Math.Sqrt(ua * ua + ub * ub);
     }
     public static bool ValidVarname(string v) {//检查v是否能作为合法物理量名
         if(keywords.Contains(v)) {//函数列表有 直接否
@@ -208,25 +316,21 @@ public class CalcArgs {//一次计算
         }
         return (valexpr.Evaluate(vals).RealValue, uncexpr.Evaluate(vals).RealValue);
     }
-    public class UserInput
-    {
+    public class UserInput {
         public string name { get; set; }
         public double value { get; set; }
         public double u { get; set; }
     }
-    public static (double, double) CalculateValue(symexpr valexpr, symexpr uncexpr, List<UserInput> inputs)
-    {
+    public static (double, double) CalculateValue(symexpr valexpr, symexpr uncexpr, List<UserInput> inputs) {
         //return (value, uncertain)
-        Dictionary<string, FloatingPoint> vals = new Dictionary<string, FloatingPoint>(inputs.Count*2);
-        foreach (var item in inputs)
-        {
+        Dictionary<string, FloatingPoint> vals = new Dictionary<string, FloatingPoint>(inputs.Count * 2);
+        foreach(var item in inputs) {
             vals[item.name] = item.value;
             vals[$"u_{item.name}"] = item.u;
         }
         return (valexpr.Evaluate(vals).RealValue, uncexpr.Evaluate(vals).RealValue);
     }
-    public static symexpr GetSymexpr(string expression)
-    {
+    public static symexpr GetSymexpr(string expression) {
         return symexpr.Parse(expression);
     }
 }

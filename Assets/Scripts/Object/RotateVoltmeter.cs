5b94d5f3de97fe9c806e562cdab1eb396f06c0a9
wsmitpwtind <wsmitpwtind@163.com>
2021-08-24 17:26:50 +0800

    qwqw

--- Assets/Scripts/Object/RotateVoltmeter.cs
+++ Assets/Scripts/Object/RotateVoltmeter.cs
@@ -43,19 +43,20 @@ public class RotateVoltmeter : HTBehaviour
             Nowin = Player_S.GetComponent<MirrorPlayer>().Nowin;
             moveable_look = Player_S.GetComponent<MirrorPlayer>().moveable_look;
             moveable_back = Player_S.GetComponent<MirrorPlayer>().moveable_back;
-            if (!Nowin&&!moveable_look)
+            if (!Nowin && !moveable_look)
             {
 
                 Player_S.GetComponent<FirstPersonController>().m_MouseLookRotate = false;
                 Player_S.GetComponent<FirstPersonController>().m_WalkSpeed = 1;
 
                 moveable_look = true;
-                mCamera = GameObject.Find("FirstPersonCharacter").gameObject.GetComponent<Camera>();
+                Player_S.GetComponent<MirrorPlayer>().moveable_look = moveable_look;
                 Player_S.GetComponent<MirrorPlayer>().updateMirror();
             }
-            else if(Nowin)
+            else if (Nowin)
             {
                 moveable_back = true;
+                Player_S.GetComponent<MirrorPlayer>().moveable_back = moveable_back;
             }
         }
         Ori_place = Player_S.GetComponent<MirrorPlayer>().Ori_place;

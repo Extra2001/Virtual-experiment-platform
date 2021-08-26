60c5789eef0111fa24c94d3dd905f41e20d41956
cbj <wojiaocbj@163.com>
2021-08-26 22:47:29 +0800

    cbj

--- Assets/Scripts/Utils/Calculate/Calculator.cs
+++ Assets/Scripts/Utils/Calculate/Calculator.cs
@@ -237,13 +237,13 @@ public static class StaticMethods {
     }
     public static string GetUaExprLatex(string varname,double s2,int k) {
         //return string.Concat(@"u_a(", varname, @")=\sqrt{\frac{\sum_{i=1}^{n}{({", varname, @"_i}-{\bar{", varname, @"}})^2}}{n(n-1)}}");
-        return string.Concat(@"u_a(", varname, @")=\sqrt{\frac{s^2}{k}}=\sqrt{", s2, "}{", k, "}}");
+        return string.Concat(@"u_a(", varname, @")=\sqrt{\frac{s^2}{k}}=\sqrt{{", s2, "}{", k, "}}");
         //下面这行是不要符号表达式的
         //return string.Concat(@"u_a(", varname, @")=\sqrt{", s2, "}{", k, "}}");
     }
     public static string GetUbExprLatex(string varname, double insterr) {
         //return string.Concat(@"u_b(", varname, @")=\frac{\Delta_{仪}}{\sqrt{3}}=\frac{", insterr.ToString(), @"}\sqrt{3}");
-        return string.Concat(@"u_b(", varname, @")=\frac{", insterr.ToString(), @"}\sqrt{3}");
+        return string.Concat(@"u_b(", varname, @")=\frac{", insterr.ToString(), @"}{\sqrt{3}}");
     }
     public static string GetUncLatex(string varname, double ua, double ub) {
         return $@"u({varname})=\sqrt{{{{u_a({varname})}}^2+{{u_b({varname})}}^2}}=\sqrt{{{ua}^2+{ub}^2}}";

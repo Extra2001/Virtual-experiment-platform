ec2e411e2bfad71df93d6222dc32af6fdca08add
flash123456789 <918127890@qq.com>
2021-08-29 12:05:35 +0800

    zjf

--- Assets/Scripts/Game/UI/PreExperiment/DealUncertainLearnUI.cs
+++ Assets/Scripts/Game/UI/PreExperiment/DealUncertainLearnUI.cs
@@ -8,6 +8,13 @@ using UnityEngine.UI;
 public class DealUncertainLearnUI : HTBehaviour
 {
     public Button BackButton;
+    public Button SureButton;
+    public Text AnswerText;
+    public InputField UserInput;
+    public GameObject Field;
+
+    public GameObject yes;
+    public GameObject no;
 
     //启用自动化
     protected override bool IsAutomate => true;
@@ -16,11 +23,58 @@ public class DealUncertainLearnUI : HTBehaviour
     void Start()
     {
         BackButton.onClick.AddListener(ClickBackButton);
+        SureButton.onClick.AddListener(ClickSureButton);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void ClickSureButton()
+    {
+        string formula;
+        string input;
+        string answer="0.1";
+
+        try
+        {
+            formula = Field.GetComponent<FormulaController>().Expression;
+
+            //answer=f(formula)
+            AnswerText.text = answer;
+
+            input = UserInput.text;
+            if (input == answer)
+            {
+                yes.transform.localScale = new Vector3(1, 1, 1);
+                yes.SetActive(true);
+                no.SetActive(false);               
+                yes.transform.DOScale(new Vector3(4, 4, 4), 1.5f).OnComplete(() => {
+                    yes.SetActive(false);
+                });
+            }
+            else
+            {
+                no.transform.localScale = new Vector3(1, 1, 1);
+                yes.SetActive(false);
+                no.SetActive(true);
+                no.transform.DOScale(new Vector3(4, 4, 4), 1.5f).OnComplete(() => {
+                    no.SetActive(false);
+                });
+            }
+        }
+        catch
+        {
+            //弹出报错提示框
+            UIAPI.Instance.ShowModel(new ModelDialogModel()
+            {
+                ShowCancel = false,
+                Title = new BindableString("错误"),
+                Message = new BindableString("输入表达式不合法，请仔细检查")
+            });
+        }
         
     }
 

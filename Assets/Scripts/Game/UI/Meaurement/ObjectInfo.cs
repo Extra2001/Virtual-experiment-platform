67997f7bad50c9655eccd7479932a4a7aea4c3f9
wsmitpwtind <wsmitpwtind@163.com>
2021-10-03 13:26:14 +0800

    qwq

--- Assets/Scripts/Game/UI/Meaurement/ObjectInfo.cs
+++ Assets/Scripts/Game/UI/Meaurement/ObjectInfo.cs
@@ -73,8 +73,9 @@ public class ObjectInfo : HTBehaviour
             // 要步进缩放的轴
             var axis = _StepAxis.selectedSegmentIndex;
             var step = Convert.ToDouble(_Step.text);
-
-
+            Debug.Log(axis);
+            Debug.Log(step);
+            Debug.Log(objectValue.Scale);
             if (axis==0)
             {
                 objectValue.LineScaleX = objectValue.LineScaleX+ objectValue.Scale  * (float)step;
@@ -87,6 +88,9 @@ public class ObjectInfo : HTBehaviour
             {
                 objectValue.LineScaleZ = objectValue.LineScaleZ+ objectValue.Scale  * (float)step;
             }
+            Debug.Log("Lx="+objectValue.LineScaleX);
+            Debug.Log("Ly="+objectValue.LineScaleY);
+            Debug.Log("Lz="+ objectValue.LineScaleZ);
             // 步进增加
         });
         _StepSub.onClick.AddListener(() =>

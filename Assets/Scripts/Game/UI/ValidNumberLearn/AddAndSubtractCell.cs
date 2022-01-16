2332e3d3232b74232f82faf183430e4d8578f768
flash123456789 <918127890@qq.com>
2022-01-16 16:41:01 +0800

    zjf

---
+++ Assets/Scripts/Game/UI/ValidNumberLearn/AddAndSubtractCell.cs
@@ -0,0 +1,101 @@
+using HT.Framework;
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddAndSubtractCell : HTBehaviour
+{
+    public int id;//对应id
+    public GameObject Root;//父节点
+
+    public Button Sign;
+    public Button SwitchButton;
+    public InputField Value;
+    public InputField Value2;
+    public InputField Digit;
+    public Sprite AddImage;
+    public Sprite SubtractImage;
+
+    public int state = 0;//0代表正常数，1代表科学计数法
+    //启用自动化
+    protected override bool IsAutomate => true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Value.onValueChanged.AddListener(value =>
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                Value.text = "0";
+            }
+            else
+            {
+                Root.GetComponent<DealCalc1>().CellValue[id].Value = decimal.Parse(value);
+            }            
+        });
+        Digit.onValueChanged.AddListener(value =>
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                Digit.text = "0";
+            }
+            else
+            {
+                Root.GetComponent<DealCalc1>().CellValue[id].Value = int.Parse(value);
+            }            
+        });
+        Value2.onValueChanged.AddListener(value =>
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                Value2.text = "0";
+            }
+            else
+            {
+                //将正常数转换为科学计数法存储
+                //Root.GetComponent<DealCalc1>().CellValue[id].Value = decimal.Parse(value);
+            }
+        });
+
+        Sign.onClick.AddListener(() =>
+        {
+            int temp = 1 - Root.GetComponent<DealCalc1>().CellValue[id].Sign;
+            Root.GetComponent<DealCalc1>().CellValue[id].Sign = temp;
+            if (temp == 0)
+            {
+                Sign.image.sprite = AddImage;
+            }
+            else
+            {
+                Sign.image.sprite = SubtractImage;
+            }
+        });
+        SwitchButton.onClick.AddListener(() =>
+        {
+            Value.text = "0";
+            Digit.text = "0";
+            Value2.text = "0";
+
+            state = 1 - state;
+            if (state == 0)
+            {
+                SwitchButton.FindChildren("Text").GetComponent<Text>().text = @"a <=> a*10^(b)";
+                Value2.gameObject.SetActive(true);
+            }
+            else
+            {
+                SwitchButton.FindChildren("Text").GetComponent<Text>().text = @"a*10^(b) <=> a";
+                Value2.gameObject.SetActive(false);
+            }
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
